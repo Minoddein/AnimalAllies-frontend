02a0d93cf3556a1733f288334b29e157a0c228bd
Scorpi-ON <s.scorpi-on@ya.ru>
2025-04-07 01:57:58 +0300

    Initial commit

---
+++ src/conf/site-metadata.ts
@@ -0,0 +1,16 @@
+const siteMetadata = {
+    name: "AnimalAllies",
+    shortName: "AnimalAllies",
+    description: "Платформа волонтёрской помощи животным",
+    repoUrl: "https://github.com/code-418-dpr/KNTista",
+    authors: { name: "Код 418", url: "https://github.com/code-418-dpr" },
+    email: "s.scorpi-on@ya.ru",
+    /* appIcon: {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+    },
+    logoLight: "/logo-light-2457x2457.png", */
+};
+
+export default siteMetadata;
