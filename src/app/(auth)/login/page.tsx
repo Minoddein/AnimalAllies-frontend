ec730512881230c16541b6db0b2c9e9c6d0dfff6
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-04-10 18:38:58 +0300

    feat: first version of login for users

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import PasswordInput from "@/components/password-input";
+import { Input } from "@heroui/react";
+
 export default function Page() {
-    return <></>;
+    return (
+        <main>
+            <form>
+                <div className="flex w-full flex-col gap-4">
+                    <Input label="Email" type="email" variant="bordered" />
+                    <PasswordInput />
+                </div>
+            </form>
+        </main>
+    );
 }
