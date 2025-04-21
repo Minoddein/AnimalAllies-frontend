036c74293676836a72524e5bc030e8e8cbe821ec
MinoddeinEz <kamazitun.jaykehd@gmail.com>
2025-04-22 00:07:53 +0300

    feat: added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,66 @@
 "use client";
 
+import Link from "next/link";
+
+import { Button } from "@heroui/button";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react";
+
 export default function App() {
-    return <main><div>Здесь будет мой веб</div></main>;
+    return (
+        <main>
+            <Header />
+            <div>Здесь будет мой веб</div>
+        </main>
+    );
+}
+
+export const AcmeLogo = () => {
+    return (
+        <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
+            <path
+                clipRule="evenodd"
+                d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+                fill="currentColor"
+                fillRule="evenodd"
+            />
+        </svg>
+    );
+};
+
+function Header() {
+    return (
+        <Navbar isBordered isBlurred={false} maxWidth="full" className="px-4">
+            <NavbarBrand>
+                <AcmeLogo />
+                <p className="font-bold text-inherit">AnimalAllies</p>
+            </NavbarBrand>
+            <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Животные
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link aria-current="page" href="#">
+                        Волонтёры
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Подать заявку
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Войти</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                        Регистрация
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    );
 }
