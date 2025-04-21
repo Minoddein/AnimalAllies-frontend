bcf559f2b8ee06b6a864a58483942eb8028df378
MinoddeinEz <kamazitun.jaykehd@gmail.com>
2025-04-22 00:23:49 +0300

    refactor: logo had been changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { PawPrintIcon as Paw } from "lucide-react";
+
 import Link from "next/link";
 
 import { Button } from "@heroui/button";
@@ -14,24 +16,19 @@ export default function App() {
     );
 }
 
-const AcmeLogo = () => {
+const AnimalAlliesLogo = () => {
     return (
-        <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-            <path
-                clipRule="evenodd"
-                d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                fill="currentColor"
-                fillRule="evenodd"
-            />
-        </svg>
+        <div className="flex items-center gap-1">
+            <Paw size={28} className="text-primary" />
+        </div>
     );
 };
 
 const Header = () => {
     return (
         <Navbar isBordered isBlurred={false} maxWidth="full" className="px-4">
-            <NavbarBrand>
-                <AcmeLogo />
+            <NavbarBrand className="flex justify-normal gap-2">
+                <AnimalAlliesLogo />
                 <p className="font-bold text-inherit">AnimalAllies</p>
             </NavbarBrand>
             <NavbarContent className="hidden gap-4 sm:flex" justify="center">
