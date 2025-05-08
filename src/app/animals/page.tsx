54bf401ace866d4ef41d3d1c015079b2ef702343
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-05-09 00:46:42 +0300

    feat: first version of animals-card

---
+++ src/app/animals/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import {useEffect, useState} from "react";
+
+import {AnimalItem} from "@/types/AnimalItem";
+import {MainCards} from "@/components/main-cards";
+import Header from "@/components/header";
+import AnimalsCards from "@/app/animals/_components/animals-cards"
+import {Tab} from "@/types/tabs";
+
+interface Paged<T> {
+    items: T[];
+    pagination: {
+        page: number;
+        pageSize: number;
+        totalItems: number;
+        totalPages: number;
+    };
+}
+
+export default function AnimalsPage() {
+    const [activeTab, setActiveTab] = useState<Tab>("animals");
+    const [isLoading] = useState(false);
+    const [animalsData, setAnimalsData] = useState<Paged<AnimalItem> | null>(null);
+    const [page, setPage] = useState(1);
+    const perPage = 12;
+
+    useEffect(() => {
+        setAnimalsData({
+            items: Array.from({length: 14}, (_, index) => ({
+                id: `id-${index}`,
+                name: `Барсик-${index + 1}`,
+                image: `https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1ueISv.img?w=1280&h=857&m=4&q=91`,
+                age: `${index + 1} лет 2 месяца`,
+                isVaccinated: index % 2 === 0,
+                humanAttitude: 'Хорошее',
+                animalsAttitude: 'Хорошее',
+                hashtags: ['#м', '#пушистый', '#домашний']
+            })),
+            pagination: {
+                page,
+                pageSize: perPage,
+                totalItems: 12,
+                totalPages: 2,
+            },
+        });
+
+    }, [page]);
+
+    const animalsPageItems = animalsData?.items ?? [];
+    const totalAnimalsPages = animalsData?.pagination.page ?? 1;
+
+    return (<>
+        <Header activeTab={activeTab} setActiveTabAction={setActiveTab}/>
+        <MainCards<AnimalItem>
+            isLoading={isLoading}
+            pageItems={animalsPageItems}
+            totalPages={totalAnimalsPages}
+            page={page}
+            setPageAction={setPage}
+            renderCardsAction={(items) => <AnimalsCards paginatedData={items}/>}
+        />
+    </>)
+}
+
+
