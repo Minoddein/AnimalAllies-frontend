bfb076efc2055a4e66c59ac13d7893052eece5e1
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-05-09 00:45:28 +0300

    feat: add types

---
+++ src/types/AnimalItem.ts
@@ -0,0 +1,10 @@
+export interface AnimalItem {
+    id: string;
+    name: string;
+    image: string;
+    age: string;
+    isVaccinated: boolean;
+    humanAttitude: string;
+    animalsAttitude: string;
+    hashtags: string[];
+}
