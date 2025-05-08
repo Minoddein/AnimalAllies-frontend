162ea0d6bd55045be3da88968a9c656d6df75287
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-05-09 00:46:18 +0300

    feat: add loading holder for pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,27 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 
-import { Providers } from "@/components/providres";
-import { AuthProvider } from "@/contexts/auth/AuthContext";
+import {Providers} from "@/components/providres";
+import {AuthProvider} from "@/contexts/auth/AuthContext";
 import "@/styles/globals.css";
+import React, {Suspense} from "react";
+import Loading from "@/app/loading";
 
-const inter = Inter({ subsets: ["latin", "cyrillic"] });
+const inter = Inter({subsets: ["latin", "cyrillic"]});
 
 export default function RootLayout({
-    children,
-}: Readonly<{
+                                       children,
+                                   }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="ru" suppressHydrationWarning>
-            <body className={inter.className}>
-                <AuthProvider>
-                    <Providers>{children}</Providers>
-                </AuthProvider>
-            </body>
+        <body className={inter.className}>
+        <AuthProvider>
+            <Suspense fallback={<Loading/>}>
+                <Providers>{children}</Providers>
+            </Suspense>
+        </AuthProvider>
+        </body>
         </html>
     );
 }
