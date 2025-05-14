ce24b01b08297936a8a33a734b1d474050588e66
Minoddein <kamazitun.jaykehd@gmail.com>
2025-05-14 16:37:45 +0300

    fix: fix navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,32 @@
-import { Inter } from "next/font/google";
+"use client";
 
+import { PawPrintIcon as Paw } from "lucide-react";
+
+import * as React from "react";
+import { useContext } from "react";
+
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import AuthForm from "@/app/_components/auth/auth-form";
+import ModalOrDrawer from "@/components/modal-or-drawer";
 import { Providers } from "@/components/providres";
-import { AuthProvider } from "@/contexts/auth/AuthContext";
+import { AuthContext, AuthProvider } from "@/contexts/auth/AuthContext";
 import "@/styles/globals.css";
+import {
+    Avatar,
+    Button,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownTrigger,
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    useDisclosure,
+} from "@heroui/react";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
@@ -15,9 +39,114 @@ export default function RootLayout({
         <html lang="ru" suppressHydrationWarning>
             <body className={inter.className}>
                 <AuthProvider>
-                    <Providers>{children}</Providers>
+                    <Providers>
+                        <Header />
+                        {children}
+                    </Providers>
                 </AuthProvider>
             </body>
         </html>
     );
 }
+
+const AnimalAlliesLogo = () => {
+    return (
+        <div className="flex items-center gap-1">
+            <Paw size={28} className="text-primary" />
+        </div>
+    );
+};
+
+const Header = () => {
+    const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+    const { accessToken, user, handleLogout } = useContext(AuthContext)!;
+    const router = useRouter();
+
+    console.log(user);
+
+    const handleOnClickLogout = async () => {
+        try {
+            await handleLogout();
+            onClose();
+        } catch (error) {
+            console.error("Logout failed:", error);
+        }
+    };
+
+    return (
+        <Navbar isBordered isBlurred={false} maxWidth="full" className="px-4">
+            <NavbarBrand className="flex justify-normal gap-2">
+                <Link href="/" className="flex justify-normal gap-2">
+                    <AnimalAlliesLogo />
+                    <p className="font-bold text-inherit">AnimalAllies</p>
+                </Link>
+            </NavbarBrand>
+            <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Животные
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link aria-current="page" href="#">
+                        Волонтёры
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Заявки
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        События
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                {/* Условный рендеринг для авторизованных/неавторизованных пользователей */}
+                {accessToken ? (
+                    <Dropdown placement="bottom-end">
+                        <DropdownTrigger>
+                            <Avatar
+                                isBordered
+                                as="button"
+                                className="transition-transform"
+                                color="secondary"
+                                name="Jason Hughes"
+                                size="sm"
+                                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                            />
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="Profile Actions" variant="flat">
+                            <DropdownItem key="profile" className="h-14 gap-2" href="/profile">
+                                <p className="font-semibold">{user!.userName}</p>
+                            </DropdownItem>
+                            <DropdownItem key="settings">Настройки</DropdownItem>
+                            <DropdownItem key="statistic">Статистика</DropdownItem>
+                            <DropdownItem key="help_and_feedback">Помощь</DropdownItem>
+                            <DropdownItem
+                                key="logout"
+                                color="danger"
+                                onPressEnd={() => {
+                                    void handleOnClickLogout();
+                                }}
+                            >
+                                Выйти
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                ) : (
+                    <NavbarItem className="flex">
+                        <Button variant="light" onPressEnd={onOpen}>
+                            Войти
+                        </Button>
+                        <ModalOrDrawer label="Вход" isOpen={isOpen} onOpenChangeAction={onOpenChange}>
+                            <AuthForm />
+                        </ModalOrDrawer>
+                    </NavbarItem>
+                )}
+            </NavbarContent>
+        </Navbar>
+    );
+};
