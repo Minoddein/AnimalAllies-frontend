25ff988e6eda30f1d99c90a67d5c28d432527a1e
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-05-16 21:50:54 +0300

    fix: fix file path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,28 @@
-import {Inter} from "next/font/google";
+import React, { Suspense } from "react";
 
-import {Providers} from "@/components/providres";
-import {AuthProvider} from "@/contexts/auth/AuthContext";
-import "@/styles/globals.css";
-import React, {Suspense} from "react";
+import { Inter } from "next/font/google";
+
 import Loading from "@/app/loading";
+import { Providers } from "@/components/providres";
+import { AuthProvider } from "@/contexts/auth/AuthContext";
+import "@/globals.css";
 
-const inter = Inter({subsets: ["latin", "cyrillic"]});
+const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="ru" suppressHydrationWarning>
-        <body className={inter.className}>
-        <AuthProvider>
-            <Suspense fallback={<Loading/>}>
-                <Providers>{children}</Providers>
-            </Suspense>
-        </AuthProvider>
-        </body>
+            <body className={inter.className}>
+                <AuthProvider>
+                    <Suspense fallback={<Loading />}>
+                        <Providers>{children}</Providers>
+                    </Suspense>
+                </AuthProvider>
+            </body>
         </html>
     );
 }
