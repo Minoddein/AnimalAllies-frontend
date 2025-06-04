24886e111904c4b910d5602fef64c81cf8c5cc27
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-06-05 00:09:30 +0300

    feat: second version of mail confirm page

--- src/app/mail-confirm/page.tsx
+++ src/app/mail-confirm/page.tsx
@@ -7,13 +7,13 @@ import { Icon } from "@iconify/react";
 export default function MailConfirmPage() {
     return (
         <>
-            <div className="min-h-[90vh] w-full content-center">
-                <div className="flex justify-center">
-                    <Icon icon="material-symbols:check" className="h-10 w-10 text-green-600" />
-                    <p className="inline-block align-bottom text-2xl">
-                        Регистрация прошла успешно! <br />
-                        Можете закрыть эту страницу
-                    </p>
+            <div className="relative z-10 flex h-full flex-col items-center justify-center px-4 text-center">
+                <div className="max-w-lg rounded-xl bg-black/40 p-8 backdrop-blur-sm">
+                    <div className="mx-auto mb-6 flex h-20 w-20 items-center justify-center rounded-full bg-green-500">
+                        <Icon icon="material-symbols:check" className="h-10 w-10" />
+                    </div>
+                    <h1 className="mb-4 text-4xl font-bold">Регистрация прошла успешно!</h1>
+                    <p className="mb-8 text-xl">Можете закрыть эту страницу</p>
                 </div>
             </div>
         </>
