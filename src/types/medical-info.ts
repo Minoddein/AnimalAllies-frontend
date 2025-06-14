7566a69bcef0ece766541e9e7e319a6cb5d7a035
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-06-14 22:45:05 +0300

    refactor: move data mok and types to folders

---
+++ src/types/medical-info.ts
@@ -0,0 +1,10 @@
+export interface MedicalInfo {
+    isVaccinated: boolean;
+    vaccinationDate: string;
+    isSterilized: boolean;
+    hasChronicDiseases: boolean;
+    medicalDescription: string;
+    needsSpecialDiet: boolean;
+    hasAllergies: boolean;
+    allergiesDescription: string;
+}
